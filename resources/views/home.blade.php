a6d50672af2909000ae0eb008ff70dd9b2cd66e0
Jonasi-Deetens <jonasideetens@hotmail.com>
2024-02-03 16:02:27 +0100

    Add info to homepage with a dummy link directing to journeys page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,12 @@
 @endsection
 
 @section('content')
-    <h1 class="title">Welcome to your journey!</h1>
+    <h1 class="title"> ~ Welcome to your journey! ~</h1>
+    <hr class="half-line">
+    <section>
+        <h2>Your destination for choosing a new destiny.</h2>
+        <p>Here we provide premade journeys which will aid you through your travels in many different countries!</p>
+        <p>You can go ahead and look at some of our offers on the 'Journeys' page.</p>
+        <a class='link' href="{{ route('home') }}">Choose a destiny</a>
+    </section>
 @endsection
